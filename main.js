d6f05d1e3a1d525c2254863370d748235cbf9ec6
Thanusan Kanagenthiran <thanushmk6@gmail.com>
2024-03-27 22:29:21 +0530

    first commit

---
+++ main.js
@@ -0,0 +1,9 @@
+import "./style.scss"
+
+const faqs = document.querySelectorAll(".faq")
+
+faqs.forEach((faq) => {
+  faq.addEventListener("click", () => {
+    faq.classList.toggle("active")
+  })
+})
